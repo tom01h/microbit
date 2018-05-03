f60b9caad4daaeb550f0a1a7b7247b1a093876c8
tom01h <tom01h@gmail.com>
2018-05-03 21:09:11 +0900

    save sensor data to sdcard on control

--- control/source/main.cpp
+++ control/source/main.cpp
@@ -1,7 +1,12 @@
 #include "MicroBit.h"
 #include "defines.h"
+#include "ff.h"
+#include "diskio.h"
 
 MicroBit uBit;
+SPI spi(MOSI, MISO, SCK);
+MicroBitPin cs(MICROBIT_ID_IO_P16, MICROBIT_PIN_P16, PIN_CAPABILITY_DIGITAL);
+DSTATUS sd_init=STA_NOINIT;
 
 MicroBitImage BS("\
         1 1 1 1 1\n\
@@ -46,43 +51,102 @@ void onData(MicroBitEvent)
 
   if(p[0]==ID*4+1){
     int i;
-    i=(int)(p[1]|(p[2]<<8)|(p[3]<<16) |(p[4]<<24));
+    i=(int)(p[1]|(p[2]<<8)|(p[3]<<16)|(p[4]<<24));
     itos(i, Lx);
-    i=(int)(p[5]|(p[6]<<8)|(p[7]<<16) |(p[8]<<24));
+    i=(int)(p[5]|(p[6]<<8)|(p[7]<<16)|(p[8]<<24));
     itos(i, Ly);
-    i=(int)(p[9]|(p[10]<<8)|(p[11]<<16) |(p[12]<<24));
+    i=(int)(p[9]|(p[10]<<8)|(p[11]<<16)|(p[12]<<24));
     itos(i, Lz);
   }
 
   if(p[0]==ID*4+2){
     int i;
-    i=(int)(p[1]|(p[2]<<8)|(p[3]<<16) |(p[4]<<24));
+    i=(int)(p[1]|(p[2]<<8)|(p[3]<<16)|(p[4]<<24));
     itos(i, Rx);
-    i=(int)(p[5]|(p[6]<<8)|(p[7]<<16) |(p[8]<<24));
+    i=(int)(p[5]|(p[6]<<8)|(p[7]<<16)|(p[8]<<24));
     itos(i, Ry);
-    i=(int)(p[9]|(p[10]<<8)|(p[11]<<16) |(p[12]<<24));
+    i=(int)(p[9]|(p[10]<<8)|(p[11]<<16)|(p[12]<<24));
     itos(i, Rz);
   }
 
 }
 
-void onButton(MicroBitEvent e)
+//  unsigned char buff[512];
+//  UINT FileSize = 512;
+TCHAR *Path = "0:/";
+FIL fil;
+FATFS fatfs;
+UINT NumBytes;
+
+void onButtonA(MicroBitEvent)
 {
-  if(e.source == MICROBIT_ID_BUTTON_A){
-    run=true;
+  DIR dir;
+  FILINFO fno;
+  FRESULT res;
+  char buf[16];
+  char *fn;
+
+  f_mount(&fatfs, Path, 0);
+  res = f_opendir(&dir, "/");
+  if(res != FR_OK){uBit.serial.printf("open dir error\r\n");}
+
+  int i = 0;
+  while(1){
+    res = f_readdir(&dir, &fno);
+    if(res != FR_OK){uBit.serial.printf("read dir error\r\n");}
+    if (res != FR_OK || fno.fname[0] == 0) break;
+    if (fno.fname[0] == '.') continue;   // ドットエントリを無視
+    fn = fno.fname;
+    if(!(fno.fattrib & AM_DIR)){         // ディレクトリを無視
+      if(fn[0]!='M') continue;
+      if(fn[1]!='B') continue;
+      if(fn[2]<'0' || fn[2]>'9') continue;
+      if(fn[3]<'0' || fn[3]>'9') continue;
+      if(fn[4]<'0' || fn[4]>'9') continue;
+      if(fn[5]<'0' || fn[5]>'9') continue;
+      if(fn[6]!='.') continue;
+      if(fn[7]!='C') continue;
+      if(fn[8]!='S') continue;
+      if(fn[9]!='V') continue;
+      if(fn[10]!=0)  continue;
+      int j=(fn[5]-'0')+(fn[4]-'0')*10+(fn[3]-'0')*100+(fn[2]-'0')*1000;
+      if(j>=i){i=j+1;}
+    }
   }
-  if(e.source == MICROBIT_ID_BUTTON_AB){
-    run=false;
-  }
+
+  buf[0]='M';
+  buf[1]='B';
+  buf[5]=i%10+'0'; i/=10;
+  buf[4]=i%10+'0'; i/=10;
+  buf[3]=i%10+'0'; i/=10;
+  buf[2]=i%10+'0';
+  buf[6]='.';
+  buf[7]='C';
+  buf[8]='S';
+  buf[9]='V';
+  buf[10]=0;
+
+  f_open(&fil, buf, FA_WRITE|FA_OPEN_ALWAYS);
+  f_write(&fil, "LX,LY,LZ,RX,RY,RZ\n", 18, &NumBytes);
+
+  uBit.serial.printf("create file : %s\r\n",buf);
+
+  run=true;
+}
+
+void onButtonAB(MicroBitEvent)
+{
+  f_close(&fil);
+
+  run=false;
 }
 
 int main()
 {
   uBit.init();
   uBit.messageBus.listen(MICROBIT_ID_RADIO, MICROBIT_RADIO_EVT_DATAGRAM, onData);
-  uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_EVT_ANY, onButton);
-  uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_EVT_ANY, onButton);
-  uBit.messageBus.listen(MICROBIT_ID_BUTTON_AB, MICROBIT_EVT_ANY, onButton);
+  uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, onButtonA);
+  uBit.messageBus.listen(MICROBIT_ID_BUTTON_AB, MICROBIT_BUTTON_EVT_CLICK, onButtonAB);
   uBit.radio.enable();
 
   PacketBuffer b(1);
@@ -92,9 +156,21 @@ int main()
       uBit.radio.datagram.send(b);
       uBit.display.print(BS);
       uBit.sleep(500);
-      uBit.serial.printf("LX=%s LY=%s LZ=%s\tRX=%s RY=%s RZ=%s\r\n",Lx,Ly,Lz,Rx,Ry,Rz);
       uBit.display.print(SS);
       uBit.sleep(500);
+      uBit.serial.printf("LX=%s LY=%s LZ=%s\tRX=%s RY=%s RZ=%s\r\n",Lx,Ly,Lz,Rx,Ry,Rz);
+      f_write(&fil, Lx,  5, &NumBytes);
+      f_write(&fil, ",", 1, &NumBytes);
+      f_write(&fil, Ly,  5, &NumBytes);
+      f_write(&fil, ",", 1, &NumBytes);
+      f_write(&fil, Lz,  5, &NumBytes);
+      f_write(&fil, ",", 1, &NumBytes);
+      f_write(&fil, Rx,  5, &NumBytes);
+      f_write(&fil, ",", 1, &NumBytes);
+      f_write(&fil, Ry,  5, &NumBytes);
+      f_write(&fil, ",", 1, &NumBytes);
+      f_write(&fil, Rz,  5, &NumBytes);
+      f_write(&fil, "\n",1, &NumBytes);
     }else{
       uBit.display.print(BB);
       uBit.sleep(100);
